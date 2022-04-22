12da057a2f2cf07143622c8952717de01f84d926
FranckyMalliet <86653867+FranckyMalliet@users.noreply.github.com>
2022-04-22 12:15:21 +0200

    /** Change log (From 22/04/2022) * ------------------------------ * Correction vcs.xml * Adding PatientHistoryControllerTest * Adding PatientHistoryServiceTest

--- microservice-patient-history/src/test/java/microservicepatienthistory/controllers/PatientHistoryControllerTest.java
+++ microservice-patient-history/src/test/java/microservicepatienthistory/controllers/PatientHistoryControllerTest.java
@@ -1,12 +1,19 @@
 package microservicepatienthistory.controllers;
 
+import microservicepatienthistory.models.PatientHistory;
 import microservicepatienthistory.services.PatientHistoryService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class PatientHistoryControllerTest {
@@ -23,5 +30,93 @@ public class PatientHistoryControllerTest {
     @BeforeEach
     public void setupMockMvc(){
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenAnUrl_ReturnAHomePageOfThisMicroservice() throws Exception {
+        mockMvc.perform(get("/patientHistory/home"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenAnUrl_ReturnAPageWithAllPatientHistories() throws Exception {
+        mockMvc.perform(get("/patientHistory/list"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenAnUrl_ReturnAPageToAddANewPatientHistory() throws Exception {
+        mockMvc.perform(get("/patientHistory/add"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenAPatientHistory_AddItToDatabase_ReturnAPageWithAllPatientHistories() throws Exception {
+        PatientHistory patientHistory = new PatientHistory();
+        patientHistory.setPatientId(100);
+        patientHistory.setNotes("No problem at all");
+
+        patientHistoryService.add(patientHistory);
+        Assertions.assertNotNull(patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+        Assertions.assertEquals("No problem at all", patientHistoryService.findById(patientHistory.getPatientHistoryId()).getNotes());
+
+        mockMvc.perform(post("/patientHistory/validate"))
+                .andExpect(status().isFound());
+
+        mockMvc.perform(get("/patientHistory/list"))
+                .andExpect(status().isOk());
+
+        patientHistoryService.delete(patientHistory.getPatientHistoryId());
+    }
+
+    @Test
+    public void givenAnId_ReturnPathToAPageWithASpecificPatientHistory() throws Exception {
+        PatientHistory patientHistory = new PatientHistory();
+        patientHistory.setPatientId(100);
+        patientHistory.setNotes("No problem at all");
+
+        patientHistoryService.add(patientHistory);
+        Assertions.assertNotNull(patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+
+        mockMvc.perform(get("/patientHistory/update/{id}", patientHistory.getPatientHistoryId()))
+                .andExpect(status().isOk());
+
+        patientHistoryService.delete(patientHistory.getPatientHistoryId());
+    }
+
+    @Test
+    public void givenNewInformationToAPatientHistory_UpdateItIntoTheDatabase_ReturnAPageWithAllPatientHistories() throws Exception {
+        PatientHistory patientHistory = new PatientHistory();
+        patientHistory.setPatientId(100);
+        patientHistory.setNotes("No problem at all");
+
+        patientHistoryService.add(patientHistory);
+        Assertions.assertNotNull(patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+
+        patientHistory.setNotes("I am in trouble !");
+        patientHistoryService.update(patientHistory);
+        Assertions.assertEquals("I am in trouble !", patientHistoryService.findById(patientHistory.getPatientHistoryId()).getNotes());
+
+        mockMvc.perform(get("/patientHistory/list"))
+                .andExpect(status().isOk());
+
+        patientHistoryService.delete(patientHistory.getPatientHistoryId());
+    }
+
+    @Test
+    public void givenAnId_DeleteAPatientHistoryInTheDatabase_ReturnAPageWithAllPatientHistories() throws Exception {
+        PatientHistory patientHistory = new PatientHistory();
+        patientHistory.setPatientId(100);
+        patientHistory.setNotes("No problem at all");
+
+        patientHistoryService.add(patientHistory);
+        Assertions.assertNotNull(patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+
+        patientHistoryService.delete(patientHistory.getPatientHistoryId());
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+        Assertions.assertEquals("Invalid patient history Id " + patientHistory.getPatientHistoryId(), exception.getMessage());
+
+        mockMvc.perform(get("/patientHistory/list"))
+                .andExpect(status().isOk());
     }
 }
--- microservice-patient-history/src/test/java/microservicepatienthistory/services/PatientHistoryServiceTest.java
+++ microservice-patient-history/src/test/java/microservicepatienthistory/services/PatientHistoryServiceTest.java
@@ -1,7 +1,42 @@
 package microservicepatienthistory.services;
 
+import microservicepatienthistory.models.PatientHistory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class PatientHistoryServiceTest {
+
+    @Autowired
+    private PatientHistoryService patientHistoryService;
+
+    @Test
+    public void patientHistoryTest(){
+        PatientHistory patientHistory = new PatientHistory();
+        patientHistory.setPatientId(100);
+        patientHistory.setNotes("No problem at all");
+
+        //SAVE
+        patientHistoryService.add(patientHistory);
+        Assertions.assertNotNull(patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+        Assertions.assertEquals("No problem at all", patientHistoryService.findById(patientHistory.getPatientHistoryId()).getNotes());
+
+        //UPDATE
+        patientHistory.setNotes("I am in trouble !");
+        patientHistoryService.update(patientHistory);
+        Assertions.assertEquals("I am in trouble !", patientHistoryService.findById(patientHistory.getPatientHistoryId()).getNotes());
+
+        //FIND
+        List<PatientHistory> patientHistoryList = patientHistoryService.findAll();
+        Assertions.assertTrue(patientHistoryList.size() > 0);
+
+        //DELETE
+        patientHistoryService.delete(patientHistory.getPatientHistoryId());
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> patientHistoryService.findById(patientHistory.getPatientHistoryId()));
+        Assertions.assertEquals("Invalid patient history Id " + patientHistory.getPatientHistoryId(), exception.getMessage());
+    }
 }
